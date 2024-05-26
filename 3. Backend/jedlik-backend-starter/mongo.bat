173c8833195e5bdea76c4d6b063dd3d9d500bfa9
muthmark <muth.mark56@gmail.com>
2024-05-26 22:08:35 +0200

    Starter reup

--- 3. Backend/jedlik-backend-starter/mongo.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 65001
-"c:\Program Files\MongoDB\Server\7.0\bin\mongoimport.exe" --uri="mongodb://localhost:27017/ncmviewpoints" --collection=locations --drop --file=locations.json --jsonArray
-"c:\Program Files\MongoDB\Server\7.0\bin\mongoimport.exe" --uri="mongodb://localhost:27017/ncmviewpoints" --collection=viewpoints --drop --file=viewpoints.json --jsonArray
-echo PLEASE KILL AND RESTART YOUR BACKEND SERVER DEV TASK IF RUNNING!
